ab2e38ef6a38431135d15abba9b9a18bfab8ee77
Pyxtroo <66847113+Pyxtroo@users.noreply.github.com>
2021-10-18 15:24:07 +0200

    Firebase googlefile

--- app/src/main/java/nl/hendriks/eindproject/MainActivity.java
+++ app/src/main/java/nl/hendriks/eindproject/MainActivity.java
@@ -14,7 +14,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(MainActivity.this, "Firebase connection succesful", Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this, "Firebase connection succesful",
+                Toast.LENGTH_LONG).show();
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.editText);
+        password = findViewById(R.id.editText2);
+        btnSingIn = findViewById(R.id.SignIn);
+        btnSingIn.setOnClickListener(new View.OnClickListener() {
+
+        @Override
+        public void onClick (View v){
+            String email = emailId.getText().toString();
+            String pwd = password.getText().toString();
+            if (email.isEmpty()) {
+                emailId.setError("Please enter a valid e-mailadress");
+                emailId.requestFocus();
+            } else if (pwd.isEmpty()) {
+                password.setError("please enter a valid password");
+                password.requestFocus();
+
+            } else if (email.isEmpty() && pwd.isEmpty()) {
+                Toast.makeText(MainActivity.this, "Fields are emty", Toast.LENGTH_SHORT).show();
+            }else if (!(email.isEmpty() && pwd.isEmpty())){
+                mFirebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "SignUp Unsuccesfull", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            startActivity(new Intent(MainActivity.this, ));
+                        }
+                    }
+                });
+            }
+            else {
+                Toast.makeText(MainActivity.this, "Error Occurred", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        });
+
+
     }
 
     public void loadRegisterScreen(View y){
