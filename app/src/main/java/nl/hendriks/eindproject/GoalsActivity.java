65a6182c8b6ce74eeb48f88cfba5af30b3ede24b
MartinVeltman <martinveltman@live.nl>
2021-10-31 13:50:13 +0100

    Namen boven in scherm aangepast goede namen geven, in het doelen scherm, rechts onderin een FAB toegevoegd met een plus logotje

--- app/src/main/java/nl/hendriks/eindproject/GoalsActivity.java
+++ app/src/main/java/nl/hendriks/eindproject/GoalsActivity.java
@@ -1,6 +1,8 @@
 package nl.hendriks.eindproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,5 +29,10 @@ public class GoalsActivity extends AppCompatActivity {
 
         recyclerGoals.setAdapter(new GoalAdapter(GoalsList));
 
+    }
+
+    public void LoadAddGoalActivity(View x){
+        Intent i = new Intent(this, AddGoalActivity.class);
+        startActivity(i);
     }
 }
