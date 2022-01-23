0bc4af5a5dacdf2d31895b3659506d90cf7c4f66
MartinVeltman <martinveltman@live.nl>
2022-01-23 19:04:47 +0100

    code wat opgeschoond

--- app/src/main/java/nl/hendriks/eindproject/RegisterActivity.java
+++ app/src/main/java/nl/hendriks/eindproject/RegisterActivity.java
@@ -6,14 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
